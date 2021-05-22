9ff1a5ec829cb47e174989aae2cff58440d0036e
webworld420 <76555387+webworld420@users.noreply.github.com>
2021-05-22 12:37:51 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,46 @@
+(function($){
+	'use strict';
+	// your code here!//
+ $(".bars").on('click',function(){
+    $(".small-menu").animate({
+      'left':'0',
+    })
+ })
+ $(".closeNav").on('click',function(){
+    $(".small-menu").animate({
+      'left':'-102%',
+    })
+ })
+
+
+// owl carousel////
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
+})(jQuery);
+
